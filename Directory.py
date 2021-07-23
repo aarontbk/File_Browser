5686cda6dcac53f72f2149e176756725c5ec3ef9
aarontbk <aarontebeka@gmail.com>
2021-07-23 11:09:19 +0300

    implemented changing directory, usage function and printing the current directory.

--- Directory.py
+++ Directory.py
@@ -5,24 +5,22 @@ import File
 class Directory:
     def __init__(self, path):
         self.path = path
-        self.name = os.path.basename(path[:-2:])
+        self.name = os.path.basename(path[:-2:]) # CR: try to improve this line
         self.creation_date = os.path.getctime(path)
 
     def __str__(self):
         return f"Name: {self.name}, creation date: {self.creation_date}"
 
-    @property
     def print_items(self) -> str:
-        num: int = 1
-        folders = []
-        files = []
-        all_files = os.listdir(self.path)
-        for file in all_files:
-            if os.path.isdir(os.path.join(self.path, file)):
-                folders.append(str(num) + " - " + file)
-                num += 1
+        folder_names = []
+        file_names = []
+        all_files_names = os.listdir(self.path)
+        for file_name in all_files_names:
+            file_path = os.path.join(self.path, file_name)
+            if os.path.isdir(file_path):
+                folder_names.append(file_name)
             else:
-                f1 = File.File(os.path.join(self.path, file))
-                files.append(f1.__str__)
+                f1 = File.File(file_path)
+                file_names.append(str(f1))
 
-        return f'Files:\n{[file for file in files]}\nFolders:\n{[folder for folder in folders]}\n'
+        return f'Files:\n{[str(file) for file in file_names]}\nFolders:\n{[folder for folder in folder_names]}\n', folder_names
--- UserInterface.py
+++ UserInterface.py
@@ -1,14 +1,36 @@
+import os
+
 import Directory
 
+
+# support following options:
+# copy file: file_name, new_file_name
+# delete file: file_name
+# rename file: file_name, new_file_name
+#
 
 class UserInterface:
     def __init__(self, path):
         self.path = path
 
     def start(self):
-        ans: str = input("What do you want to do: ")
+        que: str = "What do you want to do: "
+        ans: str = input(que)
+        self.usage(que, ans)
+
+    def usage(self, que, ans):
         while ans.lower() != "quit":
-            if ans.lower() == "dir":
-                d = Directory.Directory(self.path)
-                print(d.print_items)
-            ans = input("What do you want to do: ")
+            if ans == "dir":
+                path = self.path
+                d, f = Directory.Directory(path).print_items()
+                print(d)
+            elif ans == "path":
+                print(self.print_path(path))
+            elif ans in f:
+                path = os.path.join(path, ans)
+                d, f = Directory.Directory(path).print_items()
+                print(d)
+            ans = input(que)
+
+    def print_path(self, path):
+        return path
