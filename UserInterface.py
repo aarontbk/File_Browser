608ae1559727c5b54a61cab2eaeebf7f6b91d366
aarontbk <aarontebeka@gmail.com>
2021-07-23 17:52:23 +0300

    added try and catch to usage function

--- UserInterface.py
+++ UserInterface.py
@@ -14,31 +14,36 @@ class UserInterface:
 
     def usage(self, ans):
         while ans.lower() != "quit":
-            if ans == "dir":
-                path = self.path
-                d, folders, files = Directory.Directory(path).print_items()
-                print(d)
+            try:
+                if ans == "dir":
+                    path = self.path
+                    d, folders, files = Directory.Directory(path).print_items()
+                    print(d)
 
-            elif ans == "path":
-                print(self.print_path(path))
+                elif ans == "path":
+                    print(self.print_path(path))
 
-            elif ans in folders:
-                path = os.path.join(path, ans)
-                d, folders, files = Directory.Directory(path).print_items()
-                print(d)
+                elif ans in folders:
+                    path = os.path.join(path, ans)
+                    d, folders, files = Directory.Directory(path).print_items()
+                    print(d)
 
-            elif ans.split(" ")[0] == "rename" and ans.split(" ")[1] in files:
-                old_name = os.path.join(path, ans.split(" ")[1])
-                new_name = os.path.join(path, input("New file name: "))
-                print(self.rename(old_name, new_name))
+                elif ans.split(" ")[0] == "rename" and ans.split(" ")[1] in files:
+                    old_name = os.path.join(path, ans.split(" ")[1])
+                    new_name = os.path.join(path, input("New file name: "))
+                    print(self.rename(old_name, new_name))
 
-            elif ans.split(" ")[0] == "delete" and ans.split(" ")[1] in files:
-                print(self.delete(os.path.join(path, ans.split(" ")[1])))
+                elif ans.split(" ")[0] == "delete" and ans.split(" ")[1] in files:
+                    print(self.delete(os.path.join(path, ans.split(" ")[1])))
 
-            elif ans.split(" ")[0] == "copy" and ans.split(" ")[1] in files:
-                print(self.copy(os.path.join(path, ans.split(" ")[1]), os.path.join(path, input("New file name: "))))
-            ans = input(f"{path}: ")
+                elif ans.split(" ")[0] == "copy" and ans.split(" ")[1] in files:
+                    print(
+                        self.copy(os.path.join(path, ans.split(" ")[1]), os.path.join(path, input("New file name: "))))
 
+                ans = input(f"{path}: ")
+            except:
+                print("You must start by asking for 'dir'")
+                self.start()
 
     def print_path(self, path):
         return path
@@ -61,4 +66,3 @@ class UserInterface:
         new_file.close()
 
         return f"Copied {name} to new file: {new_name}"
-
